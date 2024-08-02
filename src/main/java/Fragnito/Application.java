93c4b9bed86a413d002836a9e1023721d7612b82
C043 <mariofragnitoph@gmail.com>
2024-08-02 15:47:15 +0200

    Removed useless code

--- src/main/java/Fragnito/Application.java
+++ src/main/java/Fragnito/Application.java
@@ -166,40 +166,5 @@ public class Application {
                 }
             }
         }
-
-
-       /* Archivio bookList = new Archivio();
-
-        Libro cadavereSquisito = new Libro(f.book().title(), isbnGen.get(), 2024, "Ernest Cline", Genre.HORROR);
-        Libro readyPlayerOne = new Libro(f.book().title(), isbnGen.get(), 2020, f.harryPotter().character(), Genre.SCI_FI);
-        Libro comedyBook = new Libro(f.book().title(), isbnGen.get(), 2020, f.funnyName().name(), Genre.COMEDY);
-
-
-        bookList.addBook(cadavereSquisito);
-        bookList.addBook(readyPlayerOne);
-        bookList.addBook(comedyBook);
-        bookList.addBook(comedyBook);
-
-        bookList.removeIsbn(2000);
-
-        bookList.isbnSearch(1001);
-
-        bookList.yearSearch(2020);
-        try {
-            bookList.authorSearch("Ernest Cline");
-        } catch (NoSuchElementException e) {
-            System.out.println("Nessun libro scritto dall'autore cercato è presente nella tua lista.");
-        }
-
-*//*
-        bookList.saveListOnDisk();
-*//*
-        bookList.printList();
-        try {
-            bookList.readListToApp("src/bookList.txt");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-*/
     }
 }

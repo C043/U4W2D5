b102b4e1aedc805df55780411d8e2d6f52d53f67
C043 <mariofragnitoph@gmail.com>
2024-08-02 09:39:51 +0200

    Adds Leggibile abstract class

---
+++ src/main/java/Fragnito/Leggibile.java
@@ -0,0 +1,18 @@
+package Fragnito;
+
+import java.util.Random;
+
+public abstract class Leggibile {
+    protected int isbnCode;
+    protected String title;
+    protected int year;
+    protected int numOfPages;
+
+    public Leggibile(String title) {
+        Random rand = new Random();
+        this.isbnCode = rand.nextInt(1000, 10000);
+        this.title = title;
+        this.year = rand.nextInt(1990, 2024);
+        this.numOfPages = rand.nextInt(100, 1000);
+    }
+}
